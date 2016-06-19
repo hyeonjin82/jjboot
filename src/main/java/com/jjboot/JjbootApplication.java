1f796bc961e310fc7ec019a600dac5e7c660974d
hyeonjinlee <qqq4343@gmail.com>
2016-06-19 19:16:27 -0400

    Add account and test

---
+++ src/main/java/com/jjboot/JjbootApplication.java
@@ -0,0 +1,12 @@
+package com.jjboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jjboot/accounts/AccountTest.java
@@ -0,0 +1,18 @@
+package com.jjboot.accounts;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+    @Test
+    public void getterSetter() {
+        Account account = new Account();
+        account.setLoginId("jin");
+        account.setPassword("1234");
+        assertThat(account.getLoginId(), is("jin"));
+    }
+}
